9e9fde9a77b49aa8710f4605ca907c9435ce8c4f
cazdemun <cplu1509@gmail.com>
2022-11-12 08:29:08 -0500

    feat: add timer data structure

--- src/timerMachine/timerMachine.ts
+++ src/timerMachine/timerMachine.ts
@@ -2,6 +2,14 @@ import { formatMillisecondsmmss, mmssToMilliseconds } from './../utils';
 import { assign, createMachine, sendParent } from "xstate";
 import alarm from '../assets/alarm10.wav';
 
+export type Timer = {
+  _id: string
+  sessionId: string
+  millisecondsOriginalGoal: number
+  sound: string
+  countable: boolean
+}
+
 export type TimerRecord = {
   _id: string
   sessionId: string
